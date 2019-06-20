67adad915d0879c51d974bffd8b3d4200b3ec9e7
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-06-20 08:10:59 -0700

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -4,4 +4,4 @@ gem install colorize
 cd
 rm -rif ali-max.key
 cp $HOME/payload5/.tool/virus/call/ali-max.key $HOME
-cp $HOME/payload5/.tool/virus/call/GSM-CALL.rb $HOME
+#cp $HOME/payload5/.tool/virus/call/GSM-CALL.rb $HOME
